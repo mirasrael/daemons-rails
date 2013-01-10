75cefe22455f8902bfdb2e7580d847bbf4c32ca7
bondarev <alexander.i.bondarev@gmail.com>
2013-01-10 13:56:31 +0400

    Remove redundant initialization block in spec helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,9 +3,3 @@ class Rails
     @root ||= Pathname.new(File.absolute_path(File.dirname(__FILE__))).join('fixtures')
   end
 end
-
-RSpec.configure do |config|
-  config.treat_symbols_as_metadata_keys_with_true_values = true
-  config.run_all_when_everything_filtered = true
-  config.filter_run :focus
-end
