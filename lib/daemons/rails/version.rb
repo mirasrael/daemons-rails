89d99c2ff254fd81821c0eb7a6e01088d958b8cc
bondarev <alexander.i.bondarev@gmail.com>
2013-01-23 18:27:03 +0400

    Fix script's template to load environment within Rails.root directory

--- lib/daemons/rails/version.rb
+++ lib/daemons/rails/version.rb
@@ -1,5 +1,5 @@
 module Daemons
   module Rails
-    VERSION = "1.1.1"
+    VERSION = "1.1.2"
   end
 end
--- lib/generators/templates/script.rb
+++ lib/generators/templates/script.rb
@@ -5,6 +5,8 @@ ENV["RAILS_ENV"] ||= "production"
 
 root = File.expand_path(File.dirname(__FILE__))
 root = File.dirname(root) until File.exists?(File.join(root, 'config'))
+Dir.chdir(root)
+
 require File.join(root, "config", "environment")
 
 $running = true
