0b73f1aac240e66b5769028fd04e5475aff24161
Yuriy Soshin <yuriy.soshin@karuna.group>
2024-02-09 11:40:50 +0300

    HOTFIX: File.exist

--- lib/generators/templates/script.rb
+++ lib/generators/templates/script.rb
@@ -4,13 +4,13 @@
 ENV["RAILS_ENV"] ||= "production"
 
 root = File.expand_path(File.dirname(__FILE__))
-root = File.dirname(root) until File.exists?(File.join(root, 'config'))
+root = File.dirname(root) until File.exist?(File.join(root, 'config'))
 Dir.chdir(root)
 
 require File.join(root, "config", "environment")
 
 $running = true
-Signal.trap("TERM") do 
+Signal.trap("TERM") do
   $running = false
 end
 
@@ -19,6 +19,6 @@ Rails.logger.auto_flushing = true if Rails.logger.respond_to?(:auto_flushing)
 while $running do
   # Replace this with your code
   Rails.logger.info "This daemon is still running at #{Time.now}.\n"
-  
+
   sleep 10
 end
