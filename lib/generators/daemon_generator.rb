40a3b64294473d29e42b2ac74eb01d64b9cbedb4
Yuriy Soshin <yuriy.soshin@karuna.group>
2024-02-09 11:43:55 +0300

    HOTFIX: File.exist

--- lib/generators/daemon_generator.rb
+++ lib/generators/daemon_generator.rb
@@ -8,7 +8,7 @@ class DaemonGenerator < Rails::Generators::NamedBase
   def generate_daemon
     daemons_dir = Daemons::Rails.configuration.daemons_directory
 
-    unless File.exists?(Rails.root.join(daemons_dir, 'daemons'))
+    unless File.exist?(Rails.root.join(daemons_dir, 'daemons'))
       copy_file "daemons", daemons_dir.join('daemons')
       chmod daemons_dir.join('daemons'), 0755
     end
@@ -19,7 +19,7 @@ class DaemonGenerator < Rails::Generators::NamedBase
     template "script_ctl", daemons_dir.join("#{file_name}_ctl")
     chmod daemons_dir.join("#{file_name}_ctl"), 0755
 
-    unless File.exists?(Rails.root.join("config", "daemons.yml"))
+    unless File.exist?(Rails.root.join("config", "daemons.yml"))
       copy_file "daemons.yml", "config/daemons.yml"
     end
   end
